6c2e43302edaf1c0a8988436ccb534cd0e14f1ca
vilavpolara <vpola226@mtroyal.ca>
2026-03-02 19:46:47 -0700

    Removed access data

--- Assignments/Assignment_Code/A2/A2_VilavPolara_LorenzoMaria.sql
+++ Assignments/Assignment_Code/A2/A2_VilavPolara_LorenzoMaria.sql
@@ -1,75 +1,3 @@
-/*
-Course:
-    course_no, primary key
-    description
-    cost
-    prerequisite, multiple
-
-Enrollment:
-    student_id, primary key
-    section_id, primary key
-    enroll_date
-    final_grade
-
-Grade:
-    student_id, primary key
-    section_id, primary key
-    grade_type_code, primary key
-    grade_code_occurrence, primary key
-    numeric_grade
-    comments
-
-Grade_Conversion:
-    letter_grade, primary key
-    grade_point
-    max_grade
-    min_grade
-
-Grade_Type:
-    grade_type_code, primary key
-    description
-
-Grade_type_weight:
-    section_id, primary key
-    grade_type_code, primary key
-    numeric_per_seciton
-    percent_of_final_grade
-    drop_lowest
-
-Instructor:
-    instructor_id, primary key
-    salutation
-    first_name
-    last_name
-    street_address
-    zip, multiple
-    phone
-
-Section:
-    section_id, primary key
-    course_no, multiple
-    section_no
-    start_date_time
-    location
-    instructor_id, multiple
-    capacity
-
-Student:
-    student_id, primary key
-    salutation
-    first_name
-    last_name
-    street_address
-    zip, multiple
-    phone
-    employer
-    registration_date
-
-Zipcode:
-    zip, primary key
-    city
-    state
-*/
 /*------------------------------------------------------------------------------
                 Assignment 2 - Querying the University Database
 
@@ -77,6 +5,11 @@ Zipcode:
                                  Shoba Ittyipe              
                       Authors: Vilav Polara, Lorenzo Maria                        
                                Due: March 2, 2026
+------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------
+Contribution Table:
+    Vilav Polara: All even number queries
+    Lorenzo Maria: All odd number requires
 ------------------------------------------------------------------------------*/
 
 -- 1. Retrieve the different descriptions of grade types that a student can 
